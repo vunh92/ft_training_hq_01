fa41b1aec43974cb64d5d726d053db6442fcb09a
vunh <huuvu1992@gmail.com>
2020-12-09 22:56:44 +0700

    chia column bằng nhau sử dụng Expanded()

--- lib/view/home_test.dart
+++ lib/view/home_test.dart
@@ -33,13 +33,13 @@ class HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Expanded(
-              // height: MediaQuery.of(context).size.height/3,
-              // color: Colors.blueAccent,
+            Container(
+              height: MediaQuery.of(context).size.height/3,
+              color: Colors.blueAccent,
               child: Stack(
                 children: <Widget>[
                   Container(
-                    height: 100,
+                    height: 80,
                     width: double.infinity,
                     decoration: BoxDecoration(
                         color: Colors.red,
@@ -68,14 +68,15 @@ class HomePageState extends State<HomePage> {
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.black54,
-                                borderRadius: BorderRadius.all(Radius.circular(10))
-                            ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          // alignment: AlignmentDirectional.bottomCenter,
+                          height: 50,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.all(Radius.circular(10))
                           ),
                         ),
                       ),
@@ -85,25 +86,18 @@ class HomePageState extends State<HomePage> {
               ),
             ),
             Container(
-              height: 2,
-              color: Colors.blue,
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.grey,
-                child: Image.network(
-                    "https://lh3.googleusercontent.com/C5W6dHX0TCIv8tO2nPiwkAZtwJEzxr6gyCySXC0hklj8G3lPP-EEeMliwAb28GF37PUeKpvG=w640-h400-e365-rj-sc0x00ffffff"
-                ),
+              height: MediaQuery.of(context).size.height/3,
+              width: double.infinity,
+              color: Colors.grey,
+              child: Image.network(
+                  "https://lh3.googleusercontent.com/C5W6dHX0TCIv8tO2nPiwkAZtwJEzxr6gyCySXC0hklj8G3lPP-EEeMliwAb28GF37PUeKpvG=w640-h400-e365-rj-sc0x00ffffff"
               ),
             ),
             Container(
-              height: 2,
-              color: Colors.blue,
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.green,
-              ),
+              height: 100,
+              // height: MediaQuery.of(context).size.height/3,
+              width: double.infinity,
+              color: Colors.green,
             ),
           ],
         )
