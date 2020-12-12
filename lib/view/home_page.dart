4556200b58d93fba1f19cc2d8baed55251e29fa9
vunh <huuvu1992@gmail.com>
2020-12-12 16:20:06 +0700

    thêm PageView

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomePage extends StatefulWidget {
   HomePage(this.title);
@@ -10,9 +11,12 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  var imageList;
+  final controller = PageController(viewportFraction: 0.8);
 
   @override
   Widget build(BuildContext context) {
+    imageList = _createImageList();
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -68,22 +72,40 @@ class HomePageState extends State<HomePage> {
                       ),
                       Expanded(
                         child: Padding(
-                          padding: const EdgeInsets.all(8.0),
+                          padding: const EdgeInsets.only(left: 8, right: 8, top: 5, bottom: 5),
                           child: Container(
-                            width: double.infinity,
                             decoration: BoxDecoration(
                                 color: Colors.black54,
                                 borderRadius: BorderRadius.all(Radius.circular(10))
                             ),
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: 5,
-                              itemBuilder: (context, index) {
-                                return ListTile(
-                                  title: Text("data"),
-                                );
-                              },
+                            child: PageView(
+                              controller: controller,
+                              children: List.generate(
+                                imageList.length,
+                                (index) => Card(
+                                  color: Colors.black54,
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 4),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4),
+                                    child: Image.network(
+                                      imageList[index],
+                                      fit: BoxFit.fitWidth,
+                                    ),
+                                  )
+                                )
+                              )
                             ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 5),
+                        child: Container(
+                          child: SmoothPageIndicator(
+                            controller: controller,
+                            count: imageList.length,
+                            effect: WormEffect(dotWidth: 25, dotHeight: 3, spacing: 4, radius: 25, activeDotColor: Colors.red),
                           ),
                         ),
                       ),
@@ -115,7 +137,20 @@ class HomePageState extends State<HomePage> {
             ),
           ],
         )
-
     );
+  }
+
+  List<String> _createImageList(){
+    List<String> list = new List();
+    list.add("https://img.freepik.com/free-photo/gold-glitter-powder-splash-background_3247-103.jpg?size=626&ext=jpg");
+    list.add("https://images.all-free-download.com/images/graphiclarge/red_christmas_horizontal_background_310556.jpg");
+    list.add("https://cdn4.vectorstock.com/i/1000x1000/87/23/abstract-blue-background-with-light-horizontal-vector-21898723.jpg");
+    list.add("https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3278279.jpg");
+    list.add("https://cdn4.vectorstock.com/i/1000x1000/20/08/horizontal-bright-color-background-vector-17302008.jpg");
+    list.add("https://image.freepik.com/free-vector/pastel-bokeh-background-horizontal_25819-688.jpg");
+    list.add("https://img.wallpapersafari.com/desktop/1680/1050/11/12/Nkx825.jpg");
+    list.add("https://i.pinimg.com/originals/42/da/a3/42daa3e3a4965ac27c7003f6fb8a11ee.jpg");
+    list.add("https://i.pinimg.com/originals/2b/96/d4/2b96d45ea17cacdbeea94270aceefc20.jpg");
+    return list;
   }
 }
